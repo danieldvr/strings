df0e3af90477c28e04a46c706b5f49b323656a2b
Sala 19A <sala19a@ceines.com.br>
2021-12-15 20:17:05 -0300

    Iniciando o projeto

---
+++ src/com/senac/IniciarProjeto.java
@@ -0,0 +1,30 @@
+package com.senac;
+
+public class IniciarProjeto {
+
+    public static void main(String[] args) {
+
+        String Usuario = "Alex";
+        int idade = 21;
+        boolean rico = true;
+        double troco = 10.16;
+
+        System.out.println(Usuario + " é Campeão desde os " + 20 + " de idade");
+        System.out.println(Usuario + " é Campeão desde os " + 20 + " de idade");
+        System.out.println(Usuario + " é Campeão desde os " + 20 + " de idade");
+        System.out.println(Usuario + " é Campeão desde os " + 20 + " de idade");
+        System.out.println("Gabriel é Campeão desde os " + 20 + " de idade");
+        System.out.println("Daniel é Campeão desde os " + 20 + " de idade");
+        System.out.println("Daniel é Campeão desde os " + (idade) + " de idade");
+        System.out.println("Daniel é Campeão desde os " + 20 + " de idade");
+        System.out.println("Daniel é Campeão desde os " + 20 + " de idade");
+        System.out.println("A turma recebeu" + troco + " reais de );
+        //System.out.println("Daniel é Campeão desde os " + (idade) + " de idade");
+
+        /*
+        System.out.println("Daniel é Campeão desde os " + (idade) + " de idade");
+        System.out.println("Daniel é Campeão desde os " + (idade) + " de idade");
+        */
+
+    }
+}
